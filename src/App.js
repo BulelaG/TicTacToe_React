813fb6dc1eb294d26e6d5b6d7546a498889ffd21
Bulela <bulelagomoshe1@gmail.com>
2024-07-23 18:25:04 +0200

    event handler with setValue('X')

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);  
+  
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
-export default function Board() { 
+export default function Board() {
   return (
     <>
       <div className="board-row">
